98a61563c9fa98bd3823f9d9944c15c2e61e39cf
Hungle2911 <122929293+Hungle2911@users.noreply.github.com>
2024-03-20 12:19:05 -0400

    feat: Add style to published tweets

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
   $("#tweet-text").on('input', function() {
     const max = 140;
-    const count = $(this).val().length;
-    const remaining = max - count
+    let count = $(this).val().length;
+    let remaining = max - count
     const counter = $(this).closest("form").find(".counter")
     counter.text(remaining).toggleClass('red-counter', remaining < 0);
   });
