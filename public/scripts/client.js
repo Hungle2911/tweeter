f9cca67ec6102b74876c233454938b811264f857
Hungle2911 <122929293+Hungle2911@users.noreply.github.com>
2024-03-21 19:06:30 -0400

    feat: add function to create and render the data as tweet articles

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,60 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+const renderTweets = function(tweets) {
+  // loops through tweets
+  for (let tweet of tweets) {
+    let $tweet = createTweetElement(tweet)
+    $('.tweet-container').append($tweet); 
+  }
+}
+const createTweetElement = function(data) {
+  const userTweet = $(`
+  <article>
+  <header class="tweet">
+    <div class="avatar">
+    <img src="${data.user.avatars}"> 
+    <span>${data.user.name}</span>
+  </div>
+    <div class="username">${data.user.handle}</div>
+  </header>
+  <main>
+    <p>${data.content.text}</p>
+    <hr>
+  </main>
+  <footer>
+    <p>${data.created_at}</p>
+    <div id="icons">
+      <i class="fa-solid fa-flag"></i>
+      <i class="fa-solid fa-repeat"></i>
+      <i class="fa-solid fa-heart"></i>
+      </div>
+  </footer>
+</article>`)
+return userTweet
+}
+renderTweets(data)
