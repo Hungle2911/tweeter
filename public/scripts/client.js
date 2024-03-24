178434e4943cecb368c57b6acb97c368b37b48d7
Hungle2911 <122929293+Hungle2911@users.noreply.github.com>
2024-03-24 18:29:45 -0400

    feat: fetch tweets from /tweets page and display the time passed since a Tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,35 +3,14 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+
+$(document).ready(() => {
 const renderTweets = function(tweets) {
+  $('.tweet-container').empty();
   // loops through tweets
   for (let tweet of tweets) {
     let $tweet = createTweetElement(tweet)
-    $('.tweet-container').append($tweet); 
+    $('.tweet-container').prepend($tweet); 
   }
 }
 const createTweetElement = function(data) {
@@ -49,7 +28,7 @@ const createTweetElement = function(data) {
     <hr>
   </main>
   <footer>
-    <p>${data.created_at}</p>
+    <p>${timeago.format(data.created_at)}</p>
     <div id="icons">
       <i class="fa-solid fa-flag"></i>
       <i class="fa-solid fa-repeat"></i>
@@ -59,4 +38,27 @@ const createTweetElement = function(data) {
 </article>`)
 return userTweet
 }
-renderTweets(data)
+const loadTweets = () => {
+  $.ajax({
+    url: '/tweets',
+    method: 'GET',
+    success: (data) => {
+      renderTweets(data);
+    }
+  })
+}
+  $('.tweet-form').on('submit', (event) => {
+    event.preventDefault();
+    const data = $('.tweet-form').serialize()
+    $.ajax({
+      url: '/tweets',
+      method: 'POST',
+      data: data,
+      success: (data) => {
+        console.log(`post request resolved successfully`);
+        loadTweets(data)
+      }
+    });
+  })
+  loadTweets()
+})
